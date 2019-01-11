91dd373c94577f3adcc5d059d66f1effb1e81bba
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-01-11 16:21:46 +0100

    Fin de session 17

---
+++ fondamentaux/nombreAttention.js
@@ -0,0 +1,8 @@
+console.log(7 / 0) // Affiche Infinity dans la console
+
+console.log('10' / 2) // Affichera 5 même si le 10 est une chaine de caractères
+
+console.log('show!' * 2)
+console.log(0.1 + 0.7) // Attention le résultat n'est pas 0.8 petite imprécision propre au langage JavaScript
+//console.log(10.toString())
+console.log((10.6575).toFixed(2))
