a9af51892e3cb03aa38386ef071f09b1ed4aa52d
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-01-11 15:30:51 +0100

    Fin de mauvaise typographie

---
+++ fondamentaux/mauvaiseTypo.js
@@ -0,0 +1,14 @@
+let maVariable = 'ok'
+console.log(maVariable)
+console.log(typeof maVariable)
+console.log('---------------------')
+
+maVariable = 3.55
+console.log(maVariable)
+console.log(typeof maVariable)
+console.log('---------------------')
+
+// Eviter les nom génériques
+let valeur = ''
+let numero = 3
+let pcd = false; // pdc veut dire Produits Chimiques Dangereux ! LOL
