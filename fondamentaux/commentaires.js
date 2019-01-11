cfa708253ec829150fe864c831fb4d0ae4c27fe9
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-01-11 13:56:51 +0100

    Sessions organisation et commentaires

---
+++ fondamentaux/commentaires.js
@@ -0,0 +1,20 @@
+// Mon premier commantaire
+
+console.log('Ligne 1')
+
+/*
+Un comantaire sur
+plusieurs lignes
+*/
+
+console.log('Ligne 2')
+/*
+ * Une chose est sure
+ * tout va bien
+ * fjkaésdkljféla
+ * alksjdféajf
+ * alksdjféajsfd
+ *
+ *
+ * Ok tout est bon j'ai trouvé la technique
+*/
---
+++ fondamentaux/organisation.js
@@ -0,0 +1,8 @@
+console.log("texte")
+
+{
+    {
+        console.log("texte 2")
+        console.log('texte 3') // Syntaxe utilisée pour le cours
+    }
+}
