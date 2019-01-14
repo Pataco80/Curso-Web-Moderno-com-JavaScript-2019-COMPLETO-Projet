f07c58f20047e8aefe2083bb23ab44bd0b8b17c9
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-01-14 11:22:49 +0100

    Fin session 19 Problème avec la methode replace

--- fondamentaux/nombreAttention.js
+++ fondamentaux/nombreAttention.js
@@ -1,7 +1,8 @@
 console.log(7 / 0) // Affiche Infinity dans la console
 
 console.log('10' / 2) // Affichera 5 même si le 10 est une chaine de caractères
-
+console.log('10' + 2) // La String prime sur la concaténation par le symbole +
+console.log('10' - 2)
 console.log('show!' * 2)
 console.log(0.1 + 0.7) // Attention le résultat n'est pas 0.8 petite imprécision propre au langage JavaScript
 //console.log(10.toString())
---
+++ fondamentaux/string.js
@@ -0,0 +1,28 @@
+console.log("--- Exemple avec double quottes ---")
+const monObjectif = "Je suis un futur Gourou JavaScript" // Double quottes ne nécéssite pas d'échapement de caractère
+const prenom = "Ricardo" // Double quottes ne nécéssite pas d'échapement de caractère
+
+console.log(monObjectif)
+console.log(monObjectif.charAt(10)) // l'espace compte comme caractère de string
+console.log(monObjectif.charAt(6)) // Affiche la lettre s
+console.log(monObjectif.charAt(80)) // Un caractère hors de la longueur de la string provoque une absence de caractère dans la console.
+console.log(monObjectif.charCodeAt(1)) // Donne l'unicode du caractère de la String à l'index 1, soit la lettre e. Si l'index n'existe pas dans la String, la valeur NaN sera retournée
+console.log(monObjectif.indexOf('i')) // Donne l'index d'un caractère dans une string. Si il y en a plusieurs, seul le premier est mentionné.
+console.log(monObjectif.substring(5)) // Affiche la string à partir de l'index donné. Le 5 correspond à la lettre i.
+console.log(monObjectif.substring(4, 12)) // Affiche la string à partir de l'index donné en premier argument et la lettre correspondant à l'argument 2 moins 1. Soit le 11ème caractère de la string, la lettre f
+console.log("Je m'appèle ".concat(prenom) + (". ").concat(monObjectif).concat("!")) // La concaténation se fait avec .concat(). Elle peut remplacer le +, mais pas de symbole + après une variable cela causera une erreur.
+console.log(monObjectif.replace(1, "t")) // Replace n'a pas fonctionné chez moi ?????
+console.log("vue,React,Angular".split(","))
+
+console.log("---------------------------")
+console.log("\n") // caractère utilisé pour faire un saut de ligne
+
+
+
+console.log("--- Exemple avec simples quottes ---")
+/* 
+ * Ci dessous vous constatez que l'usage des simples quottes 
+ * nécéssitent de faire un échapement de caractères appostrophe
+ */
+const maProfession = 'Développeur JavaScript avec Vue.js, React et si tout va bien, Angular. C\'est l\'avenir du développement 2019'
+console.log(maProfession)
