99554a37967535f60250df2f2b9e39a15b40064e
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-01-17 11:21:13 +0100

    Fin session 28

---
+++ fondamentaux/usageVar.js
@@ -0,0 +1,43 @@
+{
+    {
+        {
+            {
+                var unMot = 'Super!!!!'
+            }
+        }
+    }
+}
+console.log(unMot)
+
+
+function test() {
+    var ville = 'Renens'
+    console.log(ville)
+    console.log('------------------')
+
+}
+test()
+console.log(ville)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
