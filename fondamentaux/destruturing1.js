b94d7483433144787eec93c7077851ea136f9935
Ricardo Do Vale <ricardodovale@imac-pro-de-ricardo-1.home>
2019-01-18 13:25:26 +0100

    quelques points à éclercir. Fin.

--- fondamentaux/destruturing1.js
+++ fondamentaux/destruturing1.js
@@ -20,6 +20,8 @@ console.log(surnom, apreciation)
 const { adresse: { adresse1, numero, cp } } = personne
 console.log(adresse1, numero, cp)
 
+const { banque: { sucursale, compte } } = personne
+console.log(sucursale, compte) // Une erreur sera engendrée si une clé est inexistante. 'banque' n'existant pas il faut soit attribuer une valeur par défaut (voir ex: ligne 17) 
 
 
 
